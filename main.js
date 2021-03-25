fff5ee680e2d2b0ab7745cd30accb3f5c7237967
10reps <greyxcx@gmail.com>
2021-03-25 18:43:38 +0900

    up

---
+++ main.js
@@ -0,0 +1,29 @@
+// json파일로 부터 item정보 받아오기
+function loadTtems() {
+  return fetch("data/data.json") // fetch함수로 해당 경로의 데이터를 받아온다
+    .then((response) => response.json()) // 받아온 데이터를 json형식으로 변롼
+    .then((json) => json.items); // json파일 내부에 있는 items를 반환
+}
+
+function displayItems(items) {
+  const container = document.querySelector(".items"); // document(HTML 파일 전체를 의미) querySelector를 사용하여 HTML파일의 items 클래스를 js로 가져옴
+  container.innerHTML = items.map((item) => createHTMLString(item)).join(""); // innerHTML를 이용하여 container의 모든 HTML요소를 가져옴
+}
+
+function createHTMLString(item) {
+  return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+// 메인
+loadTtems()
+  .then((items) => {
+    console.log(items);
+    displayItems(items);
+  })
+
+  .catch(console.log);
